9e16105eb2e198d71d09f270d9821b2b93524cff
alexayyngoh <alexayyngoh@gmail.com>
2025-11-06 22:18:47 +0800

    Extend Student class and add properties
    Extend Student class from UserAccount and add fields for year of study and major.

--- src/main/entity/Student.java
+++ src/main/entity/Student.java
@@ -1,5 +1,28 @@
 package src.main.entity;
 
-public class Student {
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Student extends UserAccount {
+    private int yearOfStudy; // add final?
+    private String major;
+
+    public Student(int yearOfStudy, String major) {
+        this.yearOfStudy = yearOfStudy;
+        this.major = major;
+    }
+
+    public void viewInternship(filters) 
+
+    public void apply(op: InternshipOpportunity)
+    
+
+    public void viewStatus()
+
+    public void acceptInternship(app: InternApplication)
+
+    public void requestWithdrawal(app: InternApplication)
+
+    
     
 }
