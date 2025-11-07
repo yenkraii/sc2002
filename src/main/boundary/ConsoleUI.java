2de9095d48c757df845a26f9f67f81549bcdd694
kiara <kiaravendanooo@gmail.com>
2025-11-08 00:26:06 +0800

    implemented input validation for menu traversal

--- src/main/boundary/ConsoleUI.java
+++ src/main/boundary/ConsoleUI.java
@@ -9,6 +9,7 @@ import src.main.entity.UserAccount;
 public class ConsoleUI {
     
     private static String[] loginOptions = {"Login", "Logout"};
+    private static Scanner consoleScanner = new Scanner(System.in);
     
     public static void main(String[] args) {
         int chosenOpt = -1;
@@ -16,12 +17,17 @@ public class ConsoleUI {
 
         do{
             if(currUser == null){ // no user login
-                displayMenu(loginOptions);
-                chosenOpt = getInput();
+                // create separate login method
+                chosenOpt = getMenuInput(4);
+                continue;
             }
+            // user is logged in
+
+            
 
 
         } while (chosenOpt != -1);
+        consoleScanner.close();
         showMessage("InternSys exited!");
     }
     
@@ -35,10 +41,24 @@ public class ConsoleUI {
         showMessage(new String(new char[20]).replace("\0", "-"));
     }
 
-    public static int getInput(){
-        // TODO: VALIDATE USER INPUT
-        final Scanner sc = new Scanner(System.in);
-        return sc.nextInt();
+    public static int getMenuInput(int maxRange){
+        int result = -1; String errorMsg = ""; String exitOption = "";
+        while(true){
+            showMessage("Please enter your choice: ");
+            if(consoleScanner.hasNextInt()){
+                result = consoleScanner.nextInt();
+                if ((result > 0) && (result <= maxRange)){
+                    return result;
+                }
+                errorMsg = "Choice not listed! Please enter an option in the menu shown. ";
+            } else { // user entered a string; need to check if its the "x" option
+                exitOption = consoleScanner.next();
+                if(exitOption.equals("x")) return -1;
+                errorMsg = "Please either enter a number from 1 to " + maxRange + " or x to exit.";
+            }
+            showMessage(errorMsg);
+            consoleScanner.nextLine();
+        }
     }
 
     public static void showMessage(String msg){
