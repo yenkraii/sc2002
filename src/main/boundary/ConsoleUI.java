f1e6b9dae8c151fa2f87ead353b1471a5bab6ab8
kiara <kiaravendanooo@gmail.com>
2025-11-13 09:20:36 +0800

    framework for menu traversal in ConsoleUI

--- src/main/boundary/ConsoleUI.java
+++ src/main/boundary/ConsoleUI.java
@@ -1,61 +1,113 @@
 package src.main.boundary;
 
 import src.main.control.*;
-import src.main.entity.Student;
-import src.main.entity.UserAccount; 
 
 // should only be interacting with control classes
 // boundary class of ConsoleUI cannot directly interact with entity classes
 
 public class ConsoleUI implements IHandleIO{
 
-    //private static AuthController authController;
+    private static AuthController authController;
 
-    //ConsoleUI(AuthController ac){
-    //    authController = ac;
-    //}
+    ConsoleUI(AuthController ac){
+        authController = ac;
+    }
     
-    public static void main(String[] args) {
+    public void start(String[] args) {
         int chosenOpt = -1;
-
-        UserAccount a = new Student(null, null, null, chosenOpt, null);
-
         do{
             // menu traversal
-            //if(!(authController.getCurrentUser()  instanceof UserAccount)){
+            if((authController.getCurrentUser()  == null)){
                 // perform login
-            //} else {
+            } else {
                 // switch statement
-                switch (a.getRole()) {
-                    case "STUDENT": studentMenu(a); break;
-                    case "CENTER_STAFF": staffMenu(a); break;
-                    case "COMPANY_REP":companyMenu(a); break;
+                switch (authController.getCurrentUser().getRole()) {
+                    case "STUDENT": 
+                        studentMenu(); break;
+                    case "CENTER_STAFF": 
+                        staffMenu(); break;
+                    case "COMPANY_REP":
+                        companyMenu(); break;
                     default: // error 
                         System.out.println("Error faced!");
                         break;
                 }
-            //}
-
-
-
-
-
+            }
 
         } while (chosenOpt != -1);
         consoleScanner.close();
         System.out.println("InternSys exited!");
     }
     
-    public static void studentMenu(UserAccount account){
-        System.out.println("student");
+    public void studentMenu(){
+        final String[] lvl1StudentChoices = new String[] 
+            {"View Intern Listing", "View Applications", "Export All", "Logoff"};
+        
+        final String[] lvl2StudentChoices = new String[] 
+            {"Select", "Back"};
+        
+        displayMenu(lvl1StudentChoices);
+        switch (getMenuInput(lvl1StudentChoices.length)) {
+            case 1:
+                // view intern listing
+                break;
+            case 2:
+                // view apps
+                break;
+            case 3:
+                // export all
+                break;
+            case -1:
+                // log out
+                break;
+        }
     }
 
-    public static void companyMenu(UserAccount account){
-        System.out.println("company");
+    public void companyMenu(){
+        final String[] lvl1CompanyChoices = new String[] 
+            {"View Intern Posting", "View Applications", "Export All", "Logoff"};
+        
+        final String[] lvl2CompanyChoices = new String[] 
+            {"Select", "Back"};
+        
+        displayMenu(lvl1CompanyChoices);
+        switch (getMenuInput(lvl1CompanyChoices.length)) {
+            case 1:
+                // view intern listing
+                break;
+            case 2:
+                // view apps
+                break;
+            case 3:
+                // export all
+                break;
+            case -1:
+                // log out
+                break;
+        }
     }
     
-    public static void staffMenu(UserAccount account){
-        System.out.println("staff");
+    public void staffMenu(){
+        final String[] lvl1StaffChoices = new String[] 
+            {"View Opportunities", "View Accounts", "Export All", "Logoff"};
+        
+        final String[] lvl2StaffChoices = new String[] 
+            {"Select", "Back"};
+        
+        displayMenu(lvl1StaffChoices);
+        switch (getMenuInput(lvl1StaffChoices.length)) {
+            case 1:
+                // view Opportunities
+                break;
+            case 2:
+                // view accounts
+                break;
+            case 3:
+                // export all
+                break;
+            case -1:
+                // log out
+                break;
+        }
     }
-
 }
